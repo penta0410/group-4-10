43c5f1e160d146516728edab7eed45651cabaf40
okoking <144639739+okoking@users.noreply.github.com>
2024-04-24 14:18:44 +0900

    f

---
+++ Src/Sound/Sound.cpp
@@ -0,0 +1,104 @@
+#include"sound.h"
+#include"DxLib.h"
+
+int Sound::Bgm::handle[BGM_MAX_NUM];
+int Sound::Se::handle[SE_MAX_NUM];
+
+//BGMのパス
+char bgmSoundPath[BGM_MAX_NUM][255] =
+{
+	//音楽
+	"data/sound/bgm/title.mp3",	//タイトル
+	"data/sound/bgm/play.mp3",	//プレイ
+	"data/sound/bgm/result.mp3",	//リザルト
+};
+
+//音量
+int soundVolume_bgm[BGM_MAX_NUM] =
+{
+	70,	//タイトル
+	55,	//プレイ
+	60,	//リザルト
+};
+
+//SEのパス
+char seSoundPath[SE_MAX_NUM][255] =
+{
+	"data/sound/se/system.wav",		//システム
+	"data/sound/se/clickpanel.wav",	//パネルクリック
+	"data/sound/se/clear.wav",		//正解
+	"data/sound/se/miss.wav",		//不正解
+	"data/sound/se/damage.mp3",		//ダメージ
+};
+
+//音量
+int soundVolume_se[SE_MAX_NUM] =
+{
+	80,			//システム
+	100,		//時間制限
+	100,			//ゲーム終了
+	150,			//ガード
+	100,		//ダメージ
+};
+
+//サウンドまとめ初期化
+void Sound::Init()
+{
+	Bgm::Init();
+	Se::Init();
+}
+
+//BGMの初期化
+void Sound::Bgm::Init()
+{
+	for (int i = 0; i < BGM_MAX_NUM; i++)
+	{
+		//読み込み
+		handle[i] = LoadSoundMem(bgmSoundPath[i]);
+		//ボリュームの設定
+		ChangeVolumeSoundMem(255 * soundVolume_bgm[i] / 100, handle[i]);
+	}
+}
+//BGMの通常処理：種類
+void Sound::Bgm::Play(int type)
+{
+	//BGMの再生
+	PlaySoundMem(handle[type], DX_PLAYTYPE_LOOP, true);
+}
+//BGMの音量調節：種類,音量(ﾊﾟｰｾﾝﾃｰｼﾞ)
+void Sound::Bgm::SetVolume(int type, int volume)
+{
+	//音量の設定（0～255）（↓はvolume％の音量で出力）
+	ChangeVolumeSoundMem(255 * volume / 100, handle[type]);
+}
+//BGMの停止：種類
+void Sound::Bgm::StopSound(int type)
+{
+	//BGMの停止
+	StopSoundMem(handle[type]);
+}
+
+//SEの初期化
+void Sound::Se::Init()
+{
+	for (int i = 0; i < SE_MAX_NUM; i++)
+	{
+		//読み込み
+		handle[i] = LoadSoundMem(seSoundPath[i]);
+		//ボリュームの設定
+		int vo = soundVolume_se[i];
+		SetVolume(i, vo);
+	}
+}
+//SEの通常処理：種類
+void Sound::Se::Play(int type)
+{
+	//効果音の再生
+	PlaySoundMem(handle[type], DX_PLAYTYPE_BACK, true);
+}
+//SEの音量調節：種類,音量(ﾊﾟｰｾﾝﾃｰｼﾞ)
+void Sound::Se::SetVolume(int type, int volume)
+{
+	//音量の設定（0～255）（↓はvolume％の音量で出力）
+	ChangeVolumeSoundMem(255 * volume / 100, handle[type]);
+}
---
+++ Src/Sound/Sound.h
@@ -0,0 +1,82 @@
+#pragma once
+
+enum BGM_TYPE
+{	
+	//音楽
+	BGM_TITLE,	//タイトル画面
+	BGM_PLAY,	//プレイ画面
+	BGM_RESULT,	//リザルト画面
+
+	BGM_MAX_NUM
+};
+
+enum SE_TYPE
+{
+	SE_SYSTEM,		//システム
+	SE_CLICK_PANEL,	//パネルクリック
+	SE_CLEAR,		//正解
+	SE_MISS,		//不正解
+	SE_DAMAGE,		//ダメージ
+
+	SE_MAX_NUM
+};
+
+class Sound
+{
+public:
+	//サウンドまとめ初期化
+	static void Init();
+
+	class Bgm
+	{
+	private:
+		//音ハンドル
+		static int handle[BGM_MAX_NUM];
+
+	public:
+		//BGMの初期化
+		static void Init();
+
+		//BGMの通常処理：種類
+		static void Play(int type);
+		//BGMの音量調節：種類,音量(ﾊﾟｰｾﾝﾃｰｼﾞ)
+		static void SetVolume(int type,int volume);
+		//BGMの停止：種類
+		static void StopSound(int type);
+	};
+
+	class Se
+	{
+	private:
+		//音ハンドル
+		static int handle[SE_MAX_NUM];
+
+	public:
+		//SEの初期化
+		static void Init();
+
+		//SEの通常処理：種類
+		static void Play(int type);
+		//SEの音量調節：種類,音量(ﾊﾟｰｾﾝﾃｰｼﾞ)
+		static void SetVolume(int type,int volume);
+	};
+};
+
+
+/*使い方指南
+	//Finで使う
+	DeleteSoundMem(〇〇〇);
+	//全削除
+	InitSoundMem();
+
+	//BGMの停止
+	StopSoundMem(Hundle);
+
+	//音量の設定（0～255）（↓は80％の音量で出力）
+	ChangeVolumeSoundMem( 255 * 80 / 100, Handle );
+
+	//効果音の再生
+	PlaySoundMem(Hundle, DX_PLAYTYPE_BACK, true);
+	//BGMの再生
+	PlaySoundMem(Hundle, DX_PLAYTYPE_LOOP, true);
+*/
